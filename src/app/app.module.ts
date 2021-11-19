58d3e6a9351c233fd3ef5ce3add69aa81b971104
Vamshi Hari <vhari@innominds.com>
2021-11-19 23:54:21 +0530

    Created login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 
 
@@ -13,6 +14,7 @@ import { RegisterComponent } from './register/register.component';
     AppComponent,
     HomepageComponent,
     RegisterComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
