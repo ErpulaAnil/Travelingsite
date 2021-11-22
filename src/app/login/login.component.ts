548d61a6df813db4c13ded21f7b475aa0704019b
Vamshi Hari <vhari@innominds.com>
2021-11-22 17:51:31 +0530

    validation completed for Login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
-  selector: 'app-login',
+  selector: 'app-register',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email = "";
 
+  valid = {
+  email : true,
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
+  //validation part
+  validate(type: string): void {
+    const emailPattern = /\S+@\S+\.\S+/;
+    if (type === 'email') {
+      this.valid.email = emailPattern.test(this.email);
+    } 
+  }
 
+  //onkey function
+    onkey(event:any , type:string){
+      if( type === "email"){
+        this.email = event.target.value;
+      }
+      this.validate(type)
+    }
 }
