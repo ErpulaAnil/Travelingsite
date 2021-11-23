d0b9dc8e11beb7d0cde2d015b7fcdf7414146e3e
Vamshi Hari <vhari@innominds.com>
2021-11-23 18:25:48 +0530

    Deleted unnecessary files

--- Backend/TravelSiteApiProject/SqlConnectionClass/SqlconnectionString.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace SqlConnectionClass
-{
-    public class SqlconnectionString
-    {
-    public static string ConnectionString = @"server=IM-RT-LP-661\SQLEXPRESS;database=TravelSite;integrated security=True";
-  }
-}
