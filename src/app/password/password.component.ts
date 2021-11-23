88e066208cd7b34ffbd53339844e40f2c29fb1c7
Vamshi Hari <vhari@innominds.com>
2021-11-23 18:22:23 +0530

    Wrote validation for forget password page

--- src/app/password/password.component.ts
+++ src/app/password/password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-password',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./password.component.css']
 })
 export class PasswordComponent implements OnInit {
+  email = '';
+
+  valid ={
+    email : true,
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  validate(type: string): void {
+    const emailPattern = /\S+@\S+\.\S+/;
 
+    if (type === 'email') {
+      this.valid.email = emailPattern.test(this.email);
+    }
+  }
+
+  //onkey function
+  onkey(event: any, type: string) {
+    if (type === 'email') {
+      this.email = event.target.value;
+    }
+    this.validate(type)
+  }
+  successNotification(){
+    Swal.fire('Password has been successfully updated', 'We have been informed!', 'success')
+  }
 }
+
